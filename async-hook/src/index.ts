d79aa8b61bd670932066d8c58c263033dadf477c
yangxiaodong <yangxiaodong@qianmi.com>
2018-04-27 19:17:12 +0800

    验证下 async-hook

--- async-hook/src/index.ts
+++ async-hook/src/index.ts
@@ -1,14 +1,24 @@
 
-import async_hooks from  'async_hooks';
+import async_hooks from 'async_hooks';
 
 setTimeout(() => {
   console.log(`executionAsyncId:${async_hooks.executionAsyncId()},triggerAsyncId:${async_hooks.triggerAsyncId()}}`);
 },0);
 
 setTimeout(() => {
+  //方法调用仍在同一个hook中.
   console.log(`executionAsyncId:${async_hooks.executionAsyncId()},triggerAsyncId:${async_hooks.triggerAsyncId()}}`);
   printAsyncInfo();
 },0);
+
+let inteval  = setInterval(() => {
+  //Interval 在同一个hook中.
+  console.log(`executionAsyncId:${async_hooks.executionAsyncId()},triggerAsyncId:${async_hooks.triggerAsyncId()}}`);
+  printAsyncInfo();
+},1000);
+
+setTimeout(()=>clearInterval(inteval),5000);
+
 console.log(`executionAsyncId:${async_hooks.executionAsyncId()},triggerAsyncId:${async_hooks.triggerAsyncId()}}`);
 
 
